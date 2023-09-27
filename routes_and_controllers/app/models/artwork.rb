c89ab2b7f24f80a1b7b72315528de90024619748
Harrison Liang <liangharrison123@gmail.com>
2023-09-27 17:09:46 -0400

    phase1

--- routes_and_controllers/app/models/artwork.rb
+++ routes_and_controllers/app/models/artwork.rb
@@ -1,4 +1,39 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  image_url  :string           not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  artist_id  :bigint           not null
+#
+# Indexes
+#
+#  index_artworks_on_artist_id            (artist_id)
+#  index_artworks_on_title_and_artist_id  (title,artist_id) UNIQUE
+#
+# Foreign Keys
+#
+#  
+#
 class Artwork < ApplicationRecord
     validates :artist_id, uniqueness: {scope: :title}, presence: true
+
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
+
+    has_many :artwork_shares,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+        through: :artwork_shares,
+        source: :artwork
+
     
 end
--- routes_and_controllers/app/models/user.rb
+++ routes_and_controllers/app/models/user.rb
@@ -1,4 +1,33 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
 class User < ApplicationRecord
     validates :username, uniqueness: true, presence: true
+
+    has_many :artworks,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :Artwork
+
+    has_many :artwork_shares,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+        through: :artwork_shares,
+        source: :artwork
+
     
+        
 end
