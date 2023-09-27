de2709a0a103078c8a5b7aaaa51ccf2f2245e1f7
Harrison Liang <liangharrison123@gmail.com>
2023-09-27 15:08:16 -0400

    phase 4

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -2,14 +2,47 @@
 
 class UsersController < ApplicationController
     def index
-        render json: params
+        users = User.all
+        render json: users
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update 
+        user = User.find(params[:id])
+
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render user.errors.full_messages, status: 422
+        end
+
+    end
+
+    def destroy 
+        user = User.find_by(id: params[:id])
+
+        if user && user.destroy
+            redirect_to users_url
+        else
+            render json: {'error': "User doesn't exist"}, status: 404
+        end
+
     end
 end
