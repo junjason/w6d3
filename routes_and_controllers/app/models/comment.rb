9d21694905c0e3df51403b96ff2613c48e230e01
Jason Jun <junjason@umich.edu>
2023-09-28 15:43:45 -0400

    done with phase 5

--- routes_and_controllers/app/models/comment.rb
+++ routes_and_controllers/app/models/comment.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  artwork_id :bigint           not null
+#  author_id  :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_artwork_id  (artwork_id)
+#  index_comments_on_author_id   (author_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (artwork_id => artworks.id)
+#  fk_rails_...  (author_id => users.id)
+#
 class Comment < ApplicationRecord
     belongs_to :author,
         primary_key: :id,
