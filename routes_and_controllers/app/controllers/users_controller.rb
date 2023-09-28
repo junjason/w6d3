d771653b11e5b02400d22ffcd9611c7bc950b795
Jason Jun <junjason@umich.edu>
2023-09-28 16:02:30 -0400

    done with phase 5

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
     def index
         if params[:query]
-            users = User.where("user.username LIKE ?%", params[:query])
+            users = User.where("users.username = ?", params[:query])
         else
             users = User.all
         end
