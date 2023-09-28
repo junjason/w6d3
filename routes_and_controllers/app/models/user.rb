bc7580e0d5077dc2c09f04f4bf20e94f218e69cc
Jason Jun <junjason@umich.edu>
2023-09-28 14:13:58 -0400

    done with part 1

--- routes_and_controllers/app/models/user.rb
+++ routes_and_controllers/app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 
     has_many :artwork_shares,
         primary_key: :id,
-        foreign_key: :artist_id,
+        foreign_key: :viewer_id,
         class_name: :ArtworkShare
 
     has_many :shared_artworks,
